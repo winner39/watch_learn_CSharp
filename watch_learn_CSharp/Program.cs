c29db2bca27637501e382705d31192a85c2b4533
斯旸 陈 <342630159@qq.com>
2022-03-18 15:54:19 +0800

    添加项目文件。

---
+++ watch_learn_CSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace watch_learn_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form form = new Form();
+            form.Text = "12345";
+            form.ShowDialog();
+        }
+    }
+}
